d5ce2f9cecb4d63f98b13b78b5451a6fd4cefaf1
TeoPlow <egor294710@ya.ru>
2024-12-14 16:50:31 +0300

    в отрыве от жизни

--- src/backend/user_auth_service/src/models/Auth.py
+++ src/backend/user_auth_service/src/models/Auth.py
@@ -5,7 +5,6 @@ from sqlalchemy import (
     UUID,
     String,
     DateTime,
-    ForeignKey,
     func,
     Boolean,
     Enum,
@@ -15,21 +14,15 @@ from enum import Enum as PyEnum
 
 from src.db.database import BaseUsers
 
-class EntityType(PyEnum):
-    USER = "user"
-    ORGANIZATION = "organization"
-
 class AuthToken(BaseUsers):
     __tablename__ = "auth"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     token = Column(String(512), nullable=False, unique=True)
-    entity_id = Column(UUID(as_uuid=True), nullable=False)  # ID пользователя или организации
-    entity_type = Column(Enum(EntityType), nullable=False)  # Тип связанного объекта (пользователь/организация)
+    entity_id = Column(UUID(as_uuid=True), nullable=False)  # ID пользователя
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     expires_at = Column(DateTime(timezone=True), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
 
     # Внешние связи (необязательно)
-    user = relationship("UserProfile", foreign_keys="[AuthToken.entity_id]", viewonly=True)
-    organization = relationship("OrganizationProfile", foreign_keys="[AuthToken.entity_id]", viewonly=True)
+    user = relationship("User", foreign_keys="[AuthToken.entity_id]", viewonly=True)
