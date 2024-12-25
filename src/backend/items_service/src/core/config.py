eb4a54931f36be16a5895663c1891d59f5fa73f0
Ivan <ivanboitsov386@gmail.com>
2024-12-25 15:40:15 +0300

    Настройка подключения к бд

--- src/backend/items_service/src/core/config.py
+++ src/backend/items_service/src/core/config.py
@@ -1,9 +1,5 @@
-import yaml #type: ignore
-import argparse
-
 import os
 from dotenv import load_dotenv
-from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
@@ -14,46 +10,3 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
-
-class Config:
-    log_level: str = "info"
-    dbconnection: str = None
-    port: int = 8080
-
-    def load(self, path: str):
-        """Загружает конфигурацию из YAML-файла."""
-        try:
-            with open(path, 'r') as file:
-                config_data = yaml.safe_load(file)
-
-            # Обновляем атрибуты класса из файла конфигурации
-            if config_data:
-                for key, value in config_data.items():
-                    if hasattr(self, key):
-                        setattr(self, key, value)
-
-        except FileNotFoundError:
-            raise ValueError(f"Файл конфигурации не найден: {path}")
-        except yaml.YAMLError as e:
-            raise ValueError(f"Ошибка при разборе YAML: {e}")
-
-    def __repr__(self):
-        return f"Config(log_level={self.log_level}, dbconnection={self.dbconnection}, port={self.port})"
-
-
-def parse_args():
-    """Позваляет использовать аргумент --config для указания пути к конфигу."""
-    parser = argparse.ArgumentParser(description="Parse configuration file path.")
-    parser.add_argument(
-        "--config",
-        type=str,
-        default="./configs/dev.yml",
-        help="Path to the configuration file"
-    )
-    return parser.parse_args()
-
-"""
-Установленный конфиг приложения. Берётся из файла указанного по ключу --config или по умолчанию "./configs/dev.yml".
-"""
-cfg = Config()
-cfg.load(parse_args().config)
--- src/backend/items_service/src/core/logging.py
+++ src/backend/items_service/src/core/logging.py
@@ -1,8 +1,33 @@
+import sys
+import logging
+import logging.config
+from colorama import Fore, Style
+
+from src.core.config import LOG_LEVEL
+
+log = logging.getLogger('items_service')
+
 LOG_FORMAT = "%(asctime)s - %(name)s - [%(levelname)s] - %(message)s"
 
 LOG_DEFAULT_HANDLERS = ["console"]
 
-LOGGING = {
+# Логгер и его форматеры
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': Fore.WHITE,
+        'INFO': Fore.GREEN,
+        'WARNING': Fore.YELLOW,
+        'ERROR': Fore.RED,
+        'CRITICAL': Fore.MAGENTA
+    }
+
+    def format(self, record):
+        log_color = self.COLORS.get(record.levelname, Style.RESET_ALL)
+        record.levelname = f"{log_color}{record.levelname}{Style.RESET_ALL}"
+        record.msg = f"{log_color}{record.msg}{Style.RESET_ALL}"
+        return super().format(record)
+
+logger_config = {
     "version": 1,
     "formatters": {
         "verbose": {"format": LOG_FORMAT},
@@ -41,3 +66,4 @@ LOGGING = {
     },
     "root": {"level": "INFO", "formatter": "verbose", "handlers": LOG_DEFAULT_HANDLERS},
 }
+logging.config.dictConfig(logger_config)
