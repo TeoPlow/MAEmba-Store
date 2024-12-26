d6010b71ced282616bd36214c003f74ea853acd1
Ivan <ivanboitsov386@gmail.com>
2024-12-27 02:25:39 +0300

    Обновление main в user_auth_servcie

--- src/backend/user_auth_service/main.py
+++ src/backend/user_auth_service/main.py
@@ -16,7 +16,3 @@ async def root():
     return {"message": "Это User API для проекта MAEMBA Store"}
 
 app.add_exception_handler(SpecialException, special_exception_handler)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
