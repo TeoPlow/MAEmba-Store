e2664df00cdcaae28358c33bd56aa7d590f66bcc
Ivan <ivanboitsov386@gmail.com>
2024-12-26 17:09:16 +0300

    Карточки товаров и обновление начальной страницы
    - Также с заглушками

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,15 +1,102 @@
 "use client";
 
-import { Container, Title } from "../components/shared" 
+import { Container, Filters, ProductsGroupList } from "../components/shared" 
 
 export default function Page() {
   return(
     <main>
 
-      <Container className="mt-10">
-        <Title text="Все товары" size="lg" className="font-extrabold">
+      {/* Основная часть сайта*/}
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]">
 
-        </Title>
+          {/* Фильтрация*/}
+          <div className="w-[250px]">
+            <Filters/>
+          </div>
+
+          {/* Список товаров*/}
+          <div className="flex-1">
+            <div className=" flex flex-col gap-16">
+              <ProductsGroupList 
+                title="Все товары"
+                items={[
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                  {
+                    id: 0,
+                    name: "Комикс Маус",
+                    price: 550
+                  },
+                ]}
+              />
+            </div>
+          </div>
+        </div>
+        
       </Container>
 
     </main>
