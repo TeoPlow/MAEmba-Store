8ab5a807037e8cf3f31c50ed36faf59440f3bee6
Ivan <ivanboitsov386@gmail.com>
2024-12-27 02:40:20 +0300

    Починил работу Item_service

--- src/backend/items_service/main.py
+++ src/backend/items_service/main.py
@@ -12,7 +12,3 @@ app.include_router(items.router, prefix="/item")
 app.include_router(categories.router, prefix="/category")
 
 app.add_exception_handler(SpecialException, special_exception_handler)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- src/backend/items_service/src/core/config.py
+++ src/backend/items_service/src/core/config.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
