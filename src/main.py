94aae7699e63d2d072c77b78bbcd6df266dcb881
VlaKom17 <VAKombarov@mai.education>
2024-12-27 16:16:55 +0300

    Добавлен сервис рекомендаций

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from src.routes import recommend
+from src.core.dependencies import setup_dependencies
+from src.core.config import cfg
+from src.core.logging import LOGGING
+from rabbitmq_consumer import consume_rabbitmq
+from consumer import consume
+import asyncio
+import logging
+import uvicorn
+
+app = FastAPI(default_response_class=ORJSONResponse)
+
+app.include_router(recommend.router, prefix="")
+
+
+def start_app(config):
+    level = None
+    match config.log_level:
+        case "info":
+            level = logging.INFO
+        case "debug":
+            level = logging.DEBUG
+        case "error":
+            level = logging.ERROR
+
+    uvicorn.run(
+        "src.main:app",
+        host="localhost",
+        port=config.port,
+        log_config=LOGGING,
+        log_level=level,
+    )
+
+
+setup_dependencies(app)
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(consume())
+
+if __name__ == "__main__":
+    start_app(cfg)
