732692b15cef6b01a33d5398c25695d4f4597c61
Vladislav Kombarov <133577853+vlakom17@users.noreply.github.com>
2024-12-27 16:53:41 +0300

    Изменён consumer.py

--- src/consumer.py
+++ src/consumer.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 import aio_pika
 import uvicorn
 import asyncio
-
+from rabbitmq_consumer import process_message
 app = FastAPI()
+
 
 async def consume():
     connection = await aio_pika.connect_robust("amqp://user:password@localhost/")
@@ -12,15 +13,15 @@ async def consume():
 
     queue = await channel.declare_queue('create_item_queue', durable=True)
     queue_1 = await channel.declare_queue('create_category_queue', durable=True)
-    queue_2 = await channel.declare_queue('create_order_queue', durable=True)
+    #queue_2 = await channel.declare_queue('create_order_queue', durable=True)
 
     async def callback(message: aio_pika.IncomingMessage):
         async with message.process():
             print(message.body.decode())
 
-    await queue.consume(callback)
-    await queue_1.consume(callback)
-    await queue_2.consume(callback)
+    await queue.consume(process_message(callback))
+    await queue_1.consume(process_message(callback))
+    #await queue_2.consume(process_message(callback))
 
     while True:
         await asyncio.sleep(1)
